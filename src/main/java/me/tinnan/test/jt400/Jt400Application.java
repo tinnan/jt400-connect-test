d3a6e1e3eb42a61f06b54f2b1098f97cb94ea775
tin_nan@hotmail.com <tin_nan@hotmail.com>
2020-02-05 00:01:58 +0700

    First commit

---
+++ src/main/java/me/tinnan/test/jt400/Jt400Application.java
@@ -0,0 +1,13 @@
+package me.tinnan.test.jt400;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jt400Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jt400Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/tinnan/test/jt400/Jt400ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tinnan.test.jt400;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jt400ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
